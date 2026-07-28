38de0ae4a5e9c98dc087c8ab65231b583bb5383b
Mohammad Kashif <166394641+kasif1234@users.noreply.github.com>
2026-07-28 23:09:20 +0300

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.ts
@@ -0,0 +1,16 @@
+class Solution {
+    maxProfit(prices: number[]): number {
+        let minPrice = Infinity;
+        let maxProfit = 0;
+
+        for (const price of prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+}
