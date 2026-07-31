fb13134fa15cd3037dbc8ea62c580499e9be83bb
Mohammad Kashif <166394641+kasif1234@users.noreply.github.com>
2026-07-31 09:52:17 +0300

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.ts
@@ -0,0 +1,30 @@
+class MinStack {
+    private stack: number[];
+    private minStack: number[];
+
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(val: number): void {
+        this.stack.push(val);
+        const minVal = this.minStack.length === 0 
+            ? val 
+            : Math.min(val, this.minStack[this.minStack.length - 1]);
+        this.minStack.push(minVal);
+    }
+
+    pop(): void {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    top(): number {
+        return this.stack[this.stack.length - 1];
+    }
+
+    getMin(): number {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
