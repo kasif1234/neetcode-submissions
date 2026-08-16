2afa22e83eeacaa6c70f9f93c3c4a4230af31a2e
Mohammad Kashif <166394641+kasif1234@users.noreply.github.com>
2026-08-16 10:03:45 +0300

    Add: same-binary-tree - submission-4

---
+++ Data Structures & Algorithms/same-binary-tree/submission-4.ts
@@ -0,0 +1,7 @@
+class Solution {
+    isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+        if (!p && !q) return true;
+        if (!p || !q || p.val !== q.val) return false;
+        return this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right);
+    }
+}
