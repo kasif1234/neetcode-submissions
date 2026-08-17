291a812dcef63bfc7b25f45c22d1b86ff7aa7c09
Mohammad Kashif <166394641+kasif1234@users.noreply.github.com>
2026-08-17 07:29:58 +0300

    Add: subtree-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null) return true;
+        if (root == null) return false;
+
+        if (sameTree(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean sameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null || p.val != q.val) return false;
+        return sameTree(p.left, q.left) && sameTree(p.right, q.right);
+    }
+}
