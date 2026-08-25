b3e319baec1198fce9a17bb412b37c3a1722e25b
Mohammad Kashif <166394641+kasif1234@users.noreply.github.com>
2026-08-25 22:26:45 +0300

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.ts
@@ -0,0 +1,19 @@
+class Solution {
+    search(nums: number[], target: number): number {
+        let l = 0, r = nums.length - 1;
+        
+        while (l <= r) {
+            const mid = Math.floor((l + r) / 2);
+            
+            if (nums[mid] === target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+}
