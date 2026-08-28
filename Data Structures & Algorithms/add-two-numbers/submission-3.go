b80ef76753b87744bfe32db94c0dae29b056f8bc
Mohammad Kashif <166394641+kasif1234@users.noreply.github.com>
2026-08-28 18:28:52 +0300

    Add: add-two-numbers - submission-3

---
+++ Data Structures & Algorithms/add-two-numbers/submission-3.go
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    dummy := &ListNode{}
+    curr := dummy
+    carry := 0
+
+    for l1 != nil || l2 != nil || carry != 0 {
+        v1, v2 := 0, 0
+        if l1 != nil {
+            v1 = l1.Val
+        }
+        if l2 != nil {
+            v2 = l2.Val
+        }
+
+        total := v1 + v2 + carry
+        carry = total / 10
+        curr.Next = &ListNode{Val: total % 10}
+        curr = curr.Next
+
+        if l1 != nil {
+            l1 = l1.Next
+        }
+        if l2 != nil {
+            l2 = l2.Next
+        }
+    }
+
+    return dummy.Next
+}
