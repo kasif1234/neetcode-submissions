3a4bec1b59dce3057d6eff22f9ab238ead515644
Mohammad Kashif <166394641+kasif1234@users.noreply.github.com>
2026-08-29 12:33:22 +0300

    Add: kth-smallest-integer-in-bst - submission-1

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> stk;
+        TreeNode* curr = root;
+
+        while (!stk.empty() || curr) {
+            while (curr) {
+                stk.push(curr);
+                curr = curr->left;
+            }
+            curr = stk.top();
+            stk.pop();
+            k--;
+            if (k == 0) return curr->val;
+            curr = curr->right;
+        }
+
+        return -1;
+    }
+};
