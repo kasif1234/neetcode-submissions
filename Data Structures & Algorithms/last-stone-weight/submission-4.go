f19647dcf4cf61e576c9f9fc51f161153d54f8a6
Mohammad Kashif <166394641+kasif1234@users.noreply.github.com>
2026-09-20 22:05:00 +0300

    Add: last-stone-weight - submission-4

---
+++ Data Structures & Algorithms/last-stone-weight/submission-4.go
@@ -0,0 +1,33 @@
+import "container/heap"
+
+type maxHeap []int
+
+func (h maxHeap) Len() int            { return len(h) }
+func (h maxHeap) Less(i, j int) bool  { return h[i] > h[j] }  // > makes it a max-heap
+func (h maxHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *maxHeap) Push(x any)         { *h = append(*h, x.(int)) }
+func (h *maxHeap) Pop() any {
+    old := *h
+    n := len(old)
+    x := old[n-1]
+    *h = old[:n-1]
+    return x
+}
+
+func lastStoneWeight(stones []int) int {
+    h := maxHeap(append([]int{}, stones...))
+    heap.Init(&h)
+
+    for h.Len() > 1 {
+        y := heap.Pop(&h).(int)
+        x := heap.Pop(&h).(int)
+        if y > x {
+            heap.Push(&h, y-x)
+        }
+    }
+
+    if h.Len() == 0 {
+        return 0
+    }
+    return h[0]
+}
